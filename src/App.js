b40b1cae050fdadd0c697831c098d7a4f1a7130b
steveoh <sgourley@utah.gov>
2018-10-15 15:01:05 -0600

    refactor: use child props to reduce nesting

--- src/App.js
+++ src/App.js
@@ -2,18 +2,60 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import MapLens from './components/MapLens';
+import FindAddress from './components/dart-board/FindAddress';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const findAddress = {
+      zoomLevel: 12,
+      apiKey: 'AGRC-Explorer',
+      wkid: 3857
+    };
+
     return (
       <div className="app">
         <Header title="Atlas Utah" version="4.0.0"/>
-        <Sidebar />
         <MapLens />
+        <Sidebar>
+          Data and services provided by <a href="http://gis.utah.gov">Utah AGRC</a>
+          <p>Click a location on the map for more information</p>
+
+          <h4>Find Address</h4>
+          <div id="geocodeNode">
+            <FindAddress
+              apiKey={findAddress.apiKey}
+              wkid={findAddress.wkid}
+              zoomLevel={findAddress.zoomLevel}
+              onFind={findAddress.onFind}
+              onError={this.props.onError} />
+          </div>
+
+          <h4>Find Point of Interest</h4>
+          <div id="gnisNode"></div>
+
+          <h4>Find City</h4>
+          <div id="cityNode"></div>
+
+          <div className="panel panel-default">
+            <div className="panel-heading" role="tab">
+              <h4 className="panel-title">
+                <a role="button" data-toggle="collapse" data-target="#collapseExport" className="collapsed">
+                  Export Map
+                </a>
+              </h4>
+            </div>
+            <div id="collapseExport" className="panel-collapse collapse" role="tabpanel">
+              <div className="panel-body">
+                <div id="printDiv"></div>
+              </div>
+            </div>
+          </div>
+        </Sidebar>
       </div>
     );
   }
 }
-
-export default App;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,44 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Sidebar.css';
-import FindAddress from '../dart-board/FindAddress'
 
-class Sidebar extends React.Component {
+export default class Sidebar extends Component {
   render() {
     return (
       <div id="sideBar" className="side-bar side-bar--with-border side-bar--open">
         <div className="side-bar__padder">
-          Data and services provided by <a href="http://gis.utah.gov">Utah AGRC</a>
-          <p>Click a location on the map for more information</p>
-
-          <h4>Find Address</h4>
-          <div id="geocodeNode">
-            <FindAddress apiKey="AGRC-Explorer" wkid="26912" />
-          </div>
-
-          <h4>Find Point of Interest</h4>
-          <div id="gnisNode"></div>
-
-          <h4>Find City</h4>
-          <div id="cityNode"></div>
-
-          <div className="panel panel-default">
-            <div className="panel-heading" role="tab">
-              <h4 className="panel-title">
-                <a role="button" data-toggle="collapse" data-target="#collapseExport" className="collapsed">
-                  Export Map
-            </a>
-              </h4>
-            </div>
-            <div id="collapseExport" className="panel-collapse collapse" role="tabpanel">
-              <div className="panel-body">
-                <div id="printDiv"></div>
-              </div>
-            </div>
-          </div>
+          {this.props.children}
         </div>
       </div>
     )
   }
 }
-
-export default Sidebar;
