842f3e9249b05b3e481ce7c04735b1d3a68670b8
steveoh <sgourley@utah.gov>
2018-10-16 17:41:32 -0600

    fix: remove unused modules

--- arcgis.config.js
+++ arcgis.config.js
@@ -2,10 +2,9 @@ const path = require('path');
 
 module.exports = {
   outputPath: path.join(process.cwd(), 'public'),
-  dojoModules: ['esri/Map',
-  'esri/views/MapView',
-  'esri/Graphic',
-  'esri/geometry/Point',
-  'esri/symbols/SimpleMarkerSymbol',
-  'esri/symbols/SimpleLineSymbol'
-]};
+  dojoModules: [
+    'esri/Map',
+    'esri/views/MapView',
+    'esri/Graphic'
+  ]
+};
