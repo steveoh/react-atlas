b89b9716446c8b278207306f6a30de228f787530
steveoh <sgourley@utah.gov>
2018-10-30 19:15:05 -0600

    feat: implement layer selector
    feat: describe proptypes
    
    refactor: pass required info only

--- src/components/esrijs/MapView.js
+++ src/components/esrijs/MapView.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { loadModules, loadCss } from 'esri-loader';
+import { LayerSelectorContainer, LayerSelector } from '../../components/LayerSelector/LayerSelector';
 
 export default class ReactMapView extends Component {
   displayedZoomGraphic = null;
+  urls = {
+    landownership: 'https://gis.trustlands.utah.gov/server/' +
+      '/rest/services/Ownership/UT_SITLA_Ownership_LandOwnership_WM/FeatureServer/0'
+  };
+  discoverKey = 'career-exhibit-panel-stadium';
 
   zoomTo(zoomObj) {
     this.view.goTo(zoomObj);
@@ -18,13 +25,25 @@ export default class ReactMapView extends Component {
 
   async componentDidMount() {
     loadCss('https://js.arcgis.com/4.9/esri/css/main.css');
-    const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView']);
+    const mapRequires = [
+      'esri/Map',
+      'esri/views/MapView',
+      'esri/layers/FeatureLayer'
+    ];
+    const selectorRequires = [
+      'esri/layers/support/LOD',
+      'esri/layers/support/TileInfo',
+      'esri/layers/WebTileLayer',
+      'esri/Basemap'
+    ];
+
+    const [Map, MapView, FeatureLayer, LOD, TileInfo, WebTileLayer, Basemap] = await loadModules(mapRequires.concat(selectorRequires));
 
     this.map = new Map({
       basemap: 'dark-gray-vector'
     });
 
-    this.view = new MapView({
+    window.view = this.view = new MapView({
       container: this.mapViewDiv,
       map: this.map,
       zoom: 4,
@@ -39,6 +58,28 @@ export default class ReactMapView extends Component {
         components: ['zoom']
       }
     });
+
+    const selectorNode = document.createElement('div');
+    this.view.ui.add(selectorNode, 'top-right');
+
+    const layerSelectorOptions = {
+      view: this.view,
+      quadWord: this.discoverKey,
+      baseLayers: ['Hybrid', 'Lite', 'Terrain', 'Topo', 'Color IR'],
+      overlays: ['Address Points', {
+        Factory: FeatureLayer,
+        url: this.urls.landownership,
+        id: 'Land Ownership',
+        opacity: 0.3
+      }],
+      modules: [LOD, TileInfo, WebTileLayer, Basemap]
+    }
+
+    ReactDOM.render(
+      <LayerSelectorContainer>
+        <LayerSelector {...layerSelectorOptions}></LayerSelector>
+      </LayerSelectorContainer>,
+      selectorNode);
 
     this.view.on('click', (e) => {
       this.props.onClick(e);
