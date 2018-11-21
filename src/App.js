781ed623b4ad1eb7fa5faefc0c8b81e356c01768
steveoh <sgourley@utah.gov>
2018-11-20 18:01:28 -0700

    fix: remove space

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ export default class App extends Component {
         </IdentifyContainer>
         : null}
         <Sidebar>
-          <small>Data and services provided by <a href="https://gis.utah.gov/ ">Utah AGRC</a></small>
+          <small>Data and services provided by <a href="https://gis.utah.gov/">Utah AGRC</a></small>
           <p>Click a location on the map for more information</p>
 
           <h4>Find Address</h4>
